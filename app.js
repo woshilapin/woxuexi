31d15eb1dc29f85e752c5cd950e06fe8213a1718
Jean SIMARD <woshilapin@gmail.com>
2014-08-30 10:21:13 +0200

    Add a REST service for chinese characters

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Database
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/woxuexi', {native_parser: true});
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var chars = require('./routes/chars');
 
 var app = express();
 
@@ -14,17 +17,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'bower_components', 'font-awesome')));
 
+app.use(function(req, res, next) {
+	req.db = db;
+	next();
+});
 app.use('/', routes);
-app.use('/users', users);
+app.use('/chars', chars);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/chars.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var path = require('path');
+var mongo = require('mongoskin');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	var db = req.db;
+	db.collection('chars').find().toArray(function(err, items) {
+		if(err === null) {
+			res.status(200).json(items);
+		} else {
+			var message = {msg: err};
+			res.status(404).send(message);
+		}
+	});
+});
+
+router.get('/random', function(req, res) {
+	var db = req.db;
+	var query = {
+		$query: {
+			char: req.params.char
+		}
+	};
+	db.collection('chars').find(query).toArray(function(err, items) {
+		if(err === null) {
+			res.status(200).json(items);
+		} else {
+			var message = {msg: err};
+			res.status(404).send(message);
+		}
+	});
+});
+
+router.get('/:char', function(req, res) {
+	var db = req.db;
+	var query = {
+		$query: {
+			char: req.params.char
+		}
+	};
+	db.collection('chars').find(query).toArray(function(err, items) {
+		if(err === null) {
+			res.status(200).json(items);
+		} else {
+			var message = {msg: err};
+			res.status(404).send(message);
+		}
+	});
+});
+
+router.put('/:char', function(req, res) {
+	var db = req.db;
+	req.body._date = new Date();
+	req.body._random = Math.random();
+	var query = {
+		char: req.params.char
+	};
+	db.collection('chars').update(query, req.body, function(err, result) {
+		if(err === null && result === 1) {
+			var message = {msg: ""};
+			res.status(200).send(message);
+		} else {
+			// If resources doesn't exists create it
+			if(err === null && result === 0) {
+				db.collection('chars').insert(req.body, function(err, result) {
+					if(err === null) {
+						var message = {msg: ""};
+						res.status(201).send(message);
+					} else {
+						var message = {msg: err};
+						res.status(403).send(message);
+					}
+				});
+			} else {
+				var message = {msg: err};
+				res.status(403).send(message);
+			}
+		}
+	});
+});
+
+router.delete('/:char', function(req, res) {
+	var db = req.db;
+	var query = {
+		char: req.params.char
+	};
+	db.collection('chars').remove(query, function(err, result) {
+		if(result === 1) {
+			var message = {msg: ""};
+			res.status(204).send(message);
+		} else {
+			var message = {msg: err};
+			res.status(403).send(message);
+		}
+	});
+});
+
+module.exports = router;
