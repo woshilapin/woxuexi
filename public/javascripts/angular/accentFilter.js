fa689c0c3d77403ce4b984d1209de9b852e71c40
Jean SIMARD <woshilapin@gmail.com>
2014-09-18 20:20:45 +0200

    Split angular app into multiple files

---
+++ public/javascripts/angular/accentFilter.js
@@ -0,0 +1,21 @@
+angular.module('woxuexiApp').filter('accentFilter', [function() {
+		return function(text, char) {
+			var accents = {
+				' ': ['\u00B7', '¯', '´', 'ˇ', '`'],
+				a: ['a', '\u0101', '\u00E1', '\u01CE', '\u00E0'],
+				e: ['e', '\u0113', '\u00E9', '\u011B', '\u00E8'],
+				i: ['i', '\u012B', '\u00ED', '\u01D0', '\u00EC'],
+				o: ['o', '\u014D', '\u00F3', '\u01D2', '\u00F2'],
+				u: ['u', '\u016B', '\u00FA', '\u01D4', '\u00F9'],
+				ü: ['ü', '\u01D6', '\u01D8', '\u01DA', '\u01DC']
+			};
+			var out = '';
+			if(char === undefined) {
+				return accents[' '][text];
+			} else if(char.match(/ |a|e|i|o|u|ü/) === null) {
+				return accents[' '][text];
+			} else {
+				return accents[char][text];
+			}
+		};
+}]);
--- public/javascripts/angular/charsController.js
+++ public/javascripts/angular/charsController.js
@@ -1,5 +1,3 @@
-var woxuexiApp = angular.module('woxuexiApp', []);
-
 // When you have a callback function, pass it as an argument to this 'callback' function.
 // It will check if it's a function before calling it.
 var callback = function(next) {
@@ -8,7 +6,7 @@ var callback = function(next) {
 	}
 }
 
-woxuexiApp.controller('listwordsController', ['$scope', '$http', function($scope, $http) {
+angular.module('woxuexiApp').controller('listwordsController', ['$scope', '$http', function($scope, $http) {
 		$scope.updateWordList = function(next) {
 			var uri = 'words';
 			if($scope.search !== undefined && $scope.search !== '') {
@@ -97,48 +95,4 @@ woxuexiApp.controller('listwordsController', ['$scope', '$http', function($scope
 		};
 		$scope.updateWordList();
 		$scope.viewMode();
-}]);
-woxuexiApp.filter('accentFilter', [function() {
-		return function(text, char) {
-			var accents = {
-				' ': ['\u00B7', '¯', '´', 'ˇ', '`'],
-				a: ['a', '\u0101', '\u00E1', '\u01CE', '\u00E0'],
-				e: ['e', '\u0113', '\u00E9', '\u011B', '\u00E8'],
-				i: ['i', '\u012B', '\u00ED', '\u01D0', '\u00EC'],
-				o: ['o', '\u014D', '\u00F3', '\u01D2', '\u00F2'],
-				u: ['u', '\u016B', '\u00FA', '\u01D4', '\u00F9'],
-				ü: ['ü', '\u01D6', '\u01D8', '\u01DA', '\u01DC']
-			};
-			var out = '';
-			if(char === undefined) {
-				return accents[' '][text];
-			} else if(char.match(/ |a|e|i|o|u|ü/) === null) {
-				return accents[' '][text];
-			} else {
-				return accents[char][text];
-			}
-		};
-}]);
-woxuexiApp.filter('pinyinFilter', ['$filter', function($filter) {
-		return function(text, accent) {
-			if(text === undefined) {
-				return '';
-			}
-			if(accent === undefined) {
-				return text;
-			}
-			var out = '';
-			var currentChar = '';
-			var accentOn = false;
-			for(var index=0; index<text.length; index++) {
-				currentChar = text.charAt(index);
-				if(currentChar.match(/a|e|i|o|u/) && accentOn === false) {
-					out += $filter('accentFilter')(accent, currentChar);
-					accentOn = true;
-				} else {
-					out += currentChar;
-				}
-			}
-			return out;
-		};
 }]);
